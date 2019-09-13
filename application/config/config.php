b01ce9db7ee4b55f75d5cbc0623b0fb551adcb01
walterbernardino <wjunior.ber@gmail.com>
2019-09-13 13:33:31 -0300

    configurações inicias do codeigniter

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/SECconsult/SECconsult/';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '1OM5zlcsR7gsQfHe7wzYtbnIvu4I1lD0';
 
 /*
 |--------------------------------------------------------------------------
